35264622172c1cb6284d0fe07b9782d03065679b
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-04-01 20:46:01 +0330

    Extention method of convert to Int32 data type.

---
+++ PersonalClassLibrary/MainClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalClassLibrary
+{
+    public static class ConvertDatas
+    {
+        private static int convertedData;
+        private static bool flag;
+
+        public static bool ToInt32(this object data)
+        {
+            flag = Int32.TryParse(data.ToString(), out convertedData);
+
+            if (flag)
+            {
+                return flag;
+            }
+            else
+            {
+                return flag;
+            }
+        }
+    }
+}
