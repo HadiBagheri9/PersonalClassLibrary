2d79ec39d38e5a3982813919b25b9cc180c63667
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-04-02 18:48:06 +0330

    Extention method of ToPersianDate and ToPersianTime

--- PersonalClassLibrary/MainClass.cs
+++ PersonalClassLibrary/MainClass.cs
@@ -1,19 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace PersonalClassLibrary
 {
     public static class ConvertDatas
     {
-        private static int convertedData;
+        private static PersianCalendar persianCalendar = new PersianCalendar();
+        private static int convertedDataAsInt32;
         private static bool flag;
 
-        public static bool ToInt32(this object data)
+        public static bool CanConvertToInt32(this object data)
         {
-            return flag = int.TryParse(data.ToString(), out convertedData);
+            return flag = int.TryParse(data.ToString(), out convertedDataAsInt32);
+        }
+
+        public static int ToInt32(this object data)
+        {
+            return Convert.ToInt32(data);
+        }
+
+        public static string ToPersianDate(this DateTime data)
+        {
+            return persianCalendar.GetYear(data) + "/" + persianCalendar.GetMonth(data) + "/" + persianCalendar.GetDayOfMonth(data);
+        }
+
+        public static string ToPersianTime(this DateTime data)
+        {
+            return persianCalendar.GetHour(data) + ":" + persianCalendar.GetMinute(data) + " " + data.ToString("tt");
         }
     }
 }
