67c61e4073516c7638ae22d466d7fed20d378c2f
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-06-25 20:08:07 +0330

    Message boxes

--- PersonalClassLibrary/ConvertDatas.cs
+++ PersonalClassLibrary/ConvertDatas.cs
@@ -7,23 +7,43 @@ namespace PersonalClassLibrary
     {
         private static PersianCalendar persianCalendar = new PersianCalendar();
         private static int convertedDataAsInt32;
-        private static bool flag;
+        //private static bool flag;
 
+        /// <summary>
+        /// Validate conversion any data type to integer (32 Bit).
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static bool CanConvertToInt32(this object data)
         {
-            return flag = int.TryParse(data.ToString(), out convertedDataAsInt32);
+            return int.TryParse(data.ToString(), out convertedDataAsInt32);
         }
 
+        /// <summary>
+        /// Convert any data type to integer (32 Bit).
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static int ToInt32(this object data)
         {
             return Convert.ToInt32(data);
         }
 
+        /// <summary>
+        /// Get Persian date.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string ToPersianDate(this DateTime data)
         {
             return persianCalendar.GetYear(data) + "/" + persianCalendar.GetMonth(data) + "/" + persianCalendar.GetDayOfMonth(data);
         }
 
+        /// <summary>
+        /// Get Persian time.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string ToPersianTime(this DateTime data)
         {
             return persianCalendar.GetHour(data) + ":" + persianCalendar.GetMinute(data) + " " + data.ToString("tt");
---
+++ PersonalClassLibrary/Messages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace PersonalClassLibrary
+{
+    public static class Messages
+    {
+        private static string messageType;
+
+        /// <summary>
+        /// Show error massage box.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void MessageBoxError(this string message)
+        {
+            messageType = "Error";
+            System.Windows.MessageBox.Show(message, messageType, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        /// <summary>
+        /// Show warning massage box.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void MessageBoxWarning(this string message)
+        {
+            messageType = "Warning";
+            System.Windows.MessageBox.Show(message, messageType, MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        /// <summary>
+        /// Show information massage box.
+        /// </summary>
+        /// <param name="message"></param>
+        public static void MessageBoxInformation(this string message)
+        {
+            messageType = "Information";
+            System.Windows.MessageBox.Show(message, messageType, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
