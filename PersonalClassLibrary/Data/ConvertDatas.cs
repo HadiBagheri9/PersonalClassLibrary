8ec9afbacdc7a63837075baa0f9a8d027dd3b4f8
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-13 21:15:05 +0330

    Csv class Write FUNC

--- PersonalClassLibrary/Data/ConvertDatas.cs
+++ PersonalClassLibrary/Data/ConvertDatas.cs
@@ -6,7 +6,7 @@ namespace PersonalClassLibrary
     public static class ConvertDatas
     {
         private static PersianCalendar persianCalendar = new PersianCalendar();
-        private static int convertedDataAsInt32;
+        private static int CanconverteToInt32;
         //private static bool flag;
 
         /// <summary>
@@ -16,7 +16,7 @@ namespace PersonalClassLibrary
         /// <returns></returns>
         public static bool CanConvertToInt32(this object data)
         {
-            return int.TryParse(data.ToString(), out convertedDataAsInt32);
+            return int.TryParse(data.ToString(), out CanconverteToInt32);
         }
 
         /// <summary>
---
+++ PersonalClassLibrary/Data/Csv.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace PersonalClassLibrary.Data
+{
+    class Csv
+    {
+        public static bool WriteToFile(List<string[]> data, string separator, string path)
+        {
+            bool flag = true;
+            StreamWriter st = null;
+            try
+            {
+                st = new StreamWriter(path, false, Encoding.UTF8);
+                for (int i = 0; i < data.Count; i++)
+                {
+                    st.WriteLine(string.Join(separator, data[i]));
+                }
+            }
+            catch
+            {
+                flag = false;
+            }
+            finally
+            {
+                st.Close();
+                st.Dispose();
+            }
+
+
+
+            return flag;
+
+        }
+
+
+    }
+}
