b8edb62afb467a4462a2f6a8da14dee4c324fe22
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2025-03-30 16:08:33 +0330

    Debug ENC/DC

--- PersonalClassLibrary/Data/CryptText.cs
+++ PersonalClassLibrary/Data/CryptText.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using System.Linq;
 using System.Security.Cryptography;
+using PersonalClassLibrary.Security;
 
 namespace PersonalClassLibrary.Data
 {
     public class CryptText
     {
-        // This constant is used to determine the keysize of the encryption algorithm in bits.
-        // We divide this by 8 within the code below to get the equivalent number of bytes.
-        private const int Keysize = 256;
-
-        // This constant determines the number of iterations for the password bytes generation function.
-        private const int DerivationIterations = 1000;
-
+        
         public static string Encrypt(string text, string key)
         {
             byte[] iv = new byte[16];
@@ -48,23 +42,31 @@ namespace PersonalClassLibrary.Data
             byte[] iv = new byte[16];
             byte[] buffer = Convert.FromBase64String(text);
 
-            using (Aes aes = Aes.Create())
+            try
             {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
-
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                using (Aes aes = Aes.Create())
                 {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = iv;
+
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (MemoryStream memoryStream = new MemoryStream(buffer))
                     {
-                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
                         {
-                            return streamReader.ReadToEnd();
+                            using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                            {
+                                return streamReader.ReadToEnd();
+                            }
                         }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.MessageBoxWarning();
+                return "";
             }
         }
 
